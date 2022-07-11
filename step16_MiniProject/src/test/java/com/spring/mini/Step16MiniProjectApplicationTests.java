74a9a461427846c60f83446c548ec49218f4be14
JungYoungHun1 <ju39001@naver.com>
2022-07-11 22:26:25 +0900

    0711

---
+++ step16_MiniProject/src/test/java/com/spring/mini/Step16MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step16MiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
